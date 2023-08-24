a160d27a22ef96abf6ef3962d97cd9f097e70eb4
git-baahubali <balu.banty25@gmail.com>
2023-08-24 23:08:53 +0530

    added sidebar , added headers , added bluebox to messages , styled them

--- script.js
+++ script.js
@@ -3,22 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const messageInput = document.getElementById("messageInput");
     const sendButton = document.getElementById("sendButton");
 
-    // Establish a connection to the Socket.io server
-    const socket = io();
+    const socket = io(); // Connect to Socket.io server
 
-    sendButton.addEventListener("click", () => {
+    sendButton.addEventListener("click", sendMessage);
+    messageInput.addEventListener("keypress", handleKeyPress);
+
+    function sendMessage() {
         const message = messageInput.value;
         if (message.trim() !== "") {
-            // Emit the message to the server
-            socket.emit("chatMessage", message);
+            socket.emit("chatMessage", message); // Emit message to server
             messageInput.value = "";
         }
-    });
+    }
+
+    function handleKeyPress(event) {
+        if (event.key === "Enter") {
+            sendMessage();
+        }
+    }
 
     // Receive and display incoming messages
     socket.on("chatMessage", (message) => {
         const messageElement = document.createElement("div");
         messageElement.innerText = message;
+        messageElement.classList.add("message");
         messagesContainer.appendChild(messageElement);
         messagesContainer.scrollTop = messagesContainer.scrollHeight;
     });
